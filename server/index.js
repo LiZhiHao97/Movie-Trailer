ff6759edd29c1530c2f80ad1e16340648dce1846
lizhihao97 <940166841@qq.com>
2018-02-13 14:34:55 +0800

    feat: 增加模板引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 const Koa = require("koa");
-const ejs = require("ejs");
-const pug = require("pug");
 const app = new Koa();
-const { htmlTpl, ejsTpl, pugTpl } = require("./tpl");
-const { normal } = require("./tpl");
+const views = require("koa-views");
+const { resolve } = require("path");
+
+app.use(views(resolve(__dirname, "./views"), {
+    extension: "pug"
+}))
+
 app.use(async (ctx, next) => {
-    ctx.type = "text/html; charset=utf-8";
-    ctx.body = pug.render(pugTpl, {
+    await ctx.render("index", {
         you: "Luke",
         me: "Lee"
-    })
+    });
 })
 
 app.listen(8080);   
