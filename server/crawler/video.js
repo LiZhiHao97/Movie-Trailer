57cff1904a6f2a837e51cdbfde59706bee60e9f2
lizhihao97 <940166841@qq.com>
2018-02-17 22:10:33 +0800

    feat: 新增预告片详情页视频和封面图爬取功能

---
+++ server/crawler/video.js
@@ -0,0 +1,72 @@
+const puppeteer = require("puppeteer");
+
+const base = `https://movie.douban.com/subject/`;
+const doubanId = "26739551";
+const videoBase = `https:movie.douban.com/trailer/219491`
+
+const sleep = time => new Promise(resolve => {
+    setTimeout(resolve, time);
+});
+(async () => {
+    console.log("Start visit the target page");
+
+    const browser = await puppeteer.launch({
+         args: ["--no-sandbox"],
+         dumpio: false
+    });
+    
+    const page = await browser.newPage();
+    await page.goto(base + doubanId, {
+        waitUntil: "networkidle2"
+    })
+
+    await sleep(1000);
+
+    const result = await page.evaluate(() => {
+        const $ = window.$;
+        const it = $(".related-pic-video");
+
+        if(it && it.length > 0) {
+            const link = it.attr("href");
+            const cover = it.find("img").attr("src");
+
+            return {
+                link,
+                cover
+            }
+        }
+        
+        return {};
+    })
+    let video;
+    
+    if (result.link) {
+        await page.goto(result.link, {
+            waitUntil: "networkidle2"
+        })
+
+        await sleep(2000);
+
+        video = await page.evaluate(() => {
+            const $ = window.$;
+            const it = $("source");
+
+            if(it && it.length > 0) {
+                return it.attr("src");
+            }
+
+            return "";
+        })
+    }
+
+    const data = {
+        video,
+        doubanId,
+        cover: result.cover
+    }
+
+    browser.close();
+
+    process.send(data);
+    process.exit(0);
+})();
--- server/tasks/trailer.js
+++ server/tasks/trailer.js
@@ -3,7 +3,7 @@ const cp = require("child_process");
 const { resolve } = require("path");
 
 (async () => {
-    const script = resolve(__dirname, "../crawler/trailer-list");
+    const script = resolve(__dirname, "../crawler/video");
     const child = cp.fork(script, []);
     let invoked = false;
 
@@ -25,8 +25,6 @@ const { resolve } = require("path");
     })
 
     child.on("message", data => {
-        let result = data.result;
-
-        console.log(result);
+        console.log(data);
     })
 })();
